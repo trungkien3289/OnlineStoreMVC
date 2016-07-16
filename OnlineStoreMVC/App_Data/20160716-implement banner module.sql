25bbcc15dd852498222034ed5a7eba8010f7fa15
vspro.91@gmail.com <vspro.91@gmail.com>
2016-07-16 17:56:03 +0700

    script database

---
+++ OnlineStoreMVC/App_Data/20160716-implement banner module.sql
@@ -0,0 +1,57 @@
+﻿USE [OnlineStoreMVC]
+GO
+/****** Object:  Table [dbo].[system_Banners]    Script Date: 7/16/2016 5:53:56 PM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[system_Banners](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[ImageId] [int] NOT NULL,
+	[Name] [nvarchar](250) NULL,
+	[Type] [int] NULL,
+	[SortOrder] [int] NULL,
+	[Status] [int] NULL,
+	[CreatedBy] [uniqueidentifier] NULL,
+	[CreatedDate] [datetime] NULL,
+	[ModifiedBy] [uniqueidentifier] NULL,
+	[ModifiedDate] [datetime] NULL,
+ CONSTRAINT [PK_system_Banners] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+GO
+SET IDENTITY_INSERT [dbo].[system_Banners] ON 
+
+GO
+INSERT [dbo].[system_Banners] ([Id], [ImageId], [Name], [Type], [SortOrder], [Status], [CreatedBy], [CreatedDate], [ModifiedBy], [ModifiedDate]) VALUES (1, 40, N'Summer 1', 2, 1, 1, NULL, CAST(0x0000A645011DA9AE AS DateTime), NULL, CAST(0x0000A64501264273 AS DateTime))
+GO
+INSERT [dbo].[system_Banners] ([Id], [ImageId], [Name], [Type], [SortOrder], [Status], [CreatedBy], [CreatedDate], [ModifiedBy], [ModifiedDate]) VALUES (2, 41, N'Summer 2', 2, 2, 1, NULL, CAST(0x0000A645011F1BBE AS DateTime), NULL, CAST(0x0000A64501257FB3 AS DateTime))
+GO
+INSERT [dbo].[system_Banners] ([Id], [ImageId], [Name], [Type], [SortOrder], [Status], [CreatedBy], [CreatedDate], [ModifiedBy], [ModifiedDate]) VALUES (3, 42, N'Summer 3', 2, 3, 1, NULL, CAST(0x0000A6450125B094 AS DateTime), NULL, NULL)
+GO
+INSERT [dbo].[system_Banners] ([Id], [ImageId], [Name], [Type], [SortOrder], [Status], [CreatedBy], [CreatedDate], [ModifiedBy], [ModifiedDate]) VALUES (4, 43, N'Summer 4', 2, 4, 1, NULL, CAST(0x0000A6450125CBFA AS DateTime), NULL, NULL)
+GO
+SET IDENTITY_INSERT [dbo].[system_Banners] OFF
+GO
+ALTER TABLE [dbo].[system_Banners]  WITH CHECK ADD  CONSTRAINT [FK_system_Banners_share_Images] FOREIGN KEY([ImageId])
+REFERENCES [dbo].[share_Images] ([Id])
+GO
+ALTER TABLE [dbo].[system_Banners] CHECK CONSTRAINT [FK_system_Banners_share_Images]
+GO
+
+DELETE FROM [dbo].[system_Menu]
+SET IDENTITY_INSERT [dbo].[system_Menu] ON 
+
+INSERT [dbo].[system_Menu] ([Id], [Name], [Url], [SortOrder], [Target], [Type], [Icon], [Status], [CreatedBy], [CreatedDate], [ModifiedBy], [ModifiedDate]) VALUES (1, N'QUẢN TRỊ', N'/admin', 1, NULL, 1, N'fa-dashboard', 1, NULL, NULL, NULL, NULL)
+INSERT [dbo].[system_Menu] ([Id], [Name], [Url], [SortOrder], [Target], [Type], [Icon], [Status], [CreatedBy], [CreatedDate], [ModifiedBy], [ModifiedDate]) VALUES (2, N'THÀNH VIÊN', N'/admin/nguoi-dung', 2, NULL, 1, N'fa-user', 1, NULL, NULL, NULL, NULL)
+INSERT [dbo].[system_Menu] ([Id], [Name], [Url], [SortOrder], [Target], [Type], [Icon], [Status], [CreatedBy], [CreatedDate], [ModifiedBy], [ModifiedDate]) VALUES (3, N'SẢN PHẨM', N'/admin/san-pham', 3, NULL, 1, N'fa-barcode', 1, NULL, NULL, NULL, NULL)
+INSERT [dbo].[system_Menu] ([Id], [Name], [Url], [SortOrder], [Target], [Type], [Icon], [Status], [CreatedBy], [CreatedDate], [ModifiedBy], [ModifiedDate]) VALUES (4, N'NHÀ CUNG CẤP', N'/admin/thuong-hieu', 4, NULL, 1, N'fa-certificate', 1, NULL, NULL, NULL, NULL)
+INSERT [dbo].[system_Menu] ([Id], [Name], [Url], [SortOrder], [Target], [Type], [Icon], [Status], [CreatedBy], [CreatedDate], [ModifiedBy], [ModifiedDate]) VALUES (5, N'DANH MỤC TIN TỨC', N'/admin/danh-muc-tin-tuc', 7, NULL, 1, N'fa-server', 1, NULL, NULL, NULL, NULL)
+INSERT [dbo].[system_Menu] ([Id], [Name], [Url], [SortOrder], [Target], [Type], [Icon], [Status], [CreatedBy], [CreatedDate], [ModifiedBy], [ModifiedDate]) VALUES (7, N'DANH MỤC SẢN PHẨM', N'/admin/danh-muc-san-pham', 6, NULL, 1, N'fa-list', 1, NULL, NULL, NULL, NULL)
+INSERT [dbo].[system_Menu] ([Id], [Name], [Url], [SortOrder], [Target], [Type], [Icon], [Status], [CreatedBy], [CreatedDate], [ModifiedBy], [ModifiedDate]) VALUES (8, N'TIN TỨC', N'/admin/tin-tuc', 8, NULL, 1, N'fa-file', 1, NULL, NULL, NULL, NULL)
+INSERT [dbo].[system_Menu] ([Id], [Name], [Url], [SortOrder], [Target], [Type], [Icon], [Status], [CreatedBy], [CreatedDate], [ModifiedBy], [ModifiedDate]) VALUES (9, N'BANNER', N'/admin/banner', 9, NULL, 1, N'fa-camera', 1, NULL, NULL, NULL, NULL)
+
+SET IDENTITY_INSERT [dbo].[system_Menu] OFF
